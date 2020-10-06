36b04108669d4fe977bb4a86f3ceef9af398f49a
clf94-dev <clf94@hotmail.es>
2020-10-06 12:29:43 +0200

    added info MeetSection

---
+++ src/components/MeetSection.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function MeetSection() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default MeetSection
---
+++ src/components/MeetSectionInfo.js
@@ -0,0 +1,43 @@
+export const MeetSectionInfo = [{
+        index: 0,
+        name: 'Nikita Marks',
+        position: 'Founder & CEO',
+        description: '“It always amazes me how much talent there is in every corner of the globe.”',
+        imgAlt: 'Nikita Marks'
+    },
+    {
+        index: 1,
+        name: 'Cristian Duncan',
+        position: 'Co-founder & COO',
+        description: '“Distributed teams required unique processes. You need to approach work in a new way.”        ',
+        imgAlt: 'Cristian Duncan'
+    },
+    {
+        index: 2,
+        name: 'Cruz Hamer',
+        position: 'Co-founder & CTO',
+        description: '“Technology is at the forefront of enabling distributed teams.That is where we come in.”',
+        imgAlt: 'Cruz Hamer'
+    },
+    {
+        index: 3,
+        name: 'Drake Heaton',
+        position: 'Business Development Lead',
+        description: '“Hiring similar people from similar backgrounds is a surefire way to stunt innovation.”',
+        imgAlt: 'Drake Heaton'
+    },
+    {
+        index: 4,
+        name: 'Griffin Wise',
+        position: 'Lead Marketing',
+        description: ' “Unique perspectives shape unique products, which is what you need to survive these days.”',
+        imgAlt: 'Griffin Wise'
+    },
+    {
+        index: 5,
+        name: 'Aden Allan',
+        position: 'Head of Talent',
+        description: '“Empowered teams create truly amazing products. Set the north star and let them follow it.”',
+        imgAlt: 'Aden Allan'
+    }
+]
