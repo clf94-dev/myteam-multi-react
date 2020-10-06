b9066f38b913e9c183c6beb1efaea401b1b5a21d
clf94-dev <clf94@hotmail.es>
2020-10-06 09:54:07 +0200

    added navbar basic struct (no style)

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,27 @@
-import React from 'react'
+import React, {useState} from 'react'
+import {Link} from 'react-router-dom'
+import Logo from '../images/logo.svg'
 
 function Navbar() {
+    const [click, setClick] = useState(false);
+    const handleClick = () => setClick(!click);
+    const closeMobileMenu = () => setClick(false);
     return (
-        <div>
-            
-        </div>
+        <nav>
+            <div className="navbar-cont">
+                <Link to='/' className='navbar-logo' onClick={closeMobileMenu}>
+                    <img src={Logo} alt="myteam"/>
+                </Link>
+                <div className="menu-icon" onClick={handleClick}>
+                    <i className={click? 'fas fa-times': 'fas fa-bars'  }></i>
+                </div>
+                <ul className={click? 'nav-menu active': 'nav-menu'}>
+                    <li className="nav-item"><Link to='/' onClick={closeMobileMenu} className='nav-links' >Home</Link> </li>
+                    <li className="nav-item"><Link to='/about' onClick={closeMobileMenu} className='nav-links' >About</Link> </li>
+                </ul>
+                <Link to='/contact'  onClick={closeMobileMenu} ><button>Contact Us</button></Link>
+            </div>
+        </nav>
     )
 }
 
