f1b14847a8f7e6ccf7f39706652a9552088cc87e
clf94-dev <clf94@hotmail.es>
2020-10-07 09:30:37 +0200

    added avatar pictures to MeetSection component

--- src/components/MeetCard.jsx
+++ src/components/MeetCard.jsx
@@ -2,13 +2,23 @@ import {Grid} from '@material-ui/core'
 import React, {useState} from 'react'
 import Plus from '../images/icon-cross.svg'
 import Close from '../images/icon-close.svg'
+
+import Nikita from '../images/avatar-nikita.jpg'
+import Christian from '../images/avatar-christian.jpg'
+import Cruz from '../images/avatar-cruz.jpg'
+import Drake from '../images/avatar-drake.jpg'
+import Griffin from '../images/avatar-griffin.jpg'
+import Aden from '../images/avatar-aden.jpg'
+
 function MeetCard(props) {
+const avatars=[Nikita, Christian, Cruz, Drake, Griffin, Aden];
+
     const [click, setClick] = useState(false);
     const handleClick =( ) => setClick(!click);
     return (
         <Grid item sm={6} xs={12}>
             <Grid container className='info-card1' style={{display: !click? 'block':'none' }}>
-                <img src="" alt={props.imgAlt}/>
+                <img src={avatars[props.index]} alt={props.imgAlt}/>
                 <p className="name">{props.name}</p>
                 <p>
                     {props.position}
