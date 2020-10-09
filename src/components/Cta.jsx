abb82025b12f08959a1153739942f5232e852f4d
clf94-dev <clf94@hotmail.es>
2020-10-09 09:17:59 +0200

    added style to Cta component

--- src/components/Cta.jsx
+++ src/components/Cta.jsx
@@ -4,14 +4,14 @@ import {Link} from 'react-router-dom'
 
 function Cta() {
     return (
-        <Grid container direction='row'>
+        <Grid container direction='row' className='cta-cont'>
 
-            <Grid item sm={8} xs={12}>
+            <Grid item sm={8} xs={12} className='text-col'>
                 <h2>Ready to get started?</h2>
             </Grid>
-            <Grid item sm={4} xs={12}>
+            <Grid item sm={4} xs={12} className='btn-col'>
                 <Link to='/contact'>
-                    <button>Contact Us</button>
+                    <button className='transparent-btn'>Contact Us</button>
                 </Link>
             </Grid>
         </Grid>
