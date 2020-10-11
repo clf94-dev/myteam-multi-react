b2f9791dc5193c5b74bc61783ea9994ce98023d8
clf94-dev <clf94@hotmail.es>
2020-10-11 08:19:41 +0200

    added animation to ClientsAbout component

--- src/components/ClientsAbout.jsx
+++ src/components/ClientsAbout.jsx
@@ -5,25 +5,32 @@ import Jakarta from '../images/logo-jakarta-post.png'
 import Tech from '../images/logo-tech-radar.png'
 import TheGuardian from '../images/logo-the-guardian.png'
 import Verge from '../images/logo-the-verge.png'
+
+
+import AOS from 'aos'
+import 'aos/dist/aos.css';
+
+AOS.init();
+
 function ClientsAbout() {
     return (
         <Grid container direction='column' className='clients-cont'>
-            <h3>Some of our clients</h3>
+            <h3 data-aos='fade-right' data-aos-delay='200'>Some of our clients</h3>
             <Grid container direction='row' className='companies-row'>
                 <Grid item sm={3} xs={12} className='company-img'>
-                    <img src={Gadgets} alt="Gadgets Now"/>
+                    <img src={Gadgets} alt="Gadgets Now" data-aos='fade-left' data-aos-delay='300'/>
                 </Grid>
                 <Grid item sm={3} xs={12} className='company-img'>
-                    <img src={Jakarta}  alt="Jakarta Post"/>
+                    <img src={Jakarta}  alt="Jakarta Post" data-aos='fade-left' data-aos-delay='500'/>
                 </Grid>
                 <Grid item sm={2} xs={12} className='company-img'>
-                    <img src={Tech}  alt="Tech Radar"/>
+                    <img src={Tech}  alt="Tech Radar" data-aos='fade-left' data-aos-delay='700'/>
                 </Grid>
                 <Grid item sm={2} xs={12} className='company-img'>
-                    <img src={TheGuardian}  alt="The Guardian"/>
+                    <img src={TheGuardian}  alt="The Guardian" data-aos='fade-left' data-aos-delay='900'/>
                 </Grid>
                 <Grid item sm={2} xs={12} className='company-img'>
-                    <img src={Verge}  alt="The Verge"/>
+                    <img src={Verge}  alt="The Verge" data-aos='fade-left' data-aos-delay='1100'/>
                 </Grid>
             </Grid>
         </Grid>
