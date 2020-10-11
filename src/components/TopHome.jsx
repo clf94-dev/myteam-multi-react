3d206c974c117e3e0e4308f5551b72d622e84913
clf94-dev <clf94@hotmail.es>
2020-10-11 07:45:34 +0200

    added animation TopHome component

--- src/components/TopHome.jsx
+++ src/components/TopHome.jsx
@@ -1,13 +1,18 @@
 import {Grid} from '@material-ui/core'
 import React from 'react'
 
+import AOS from 'aos'
+import 'aos/dist/aos.css';
+
+AOS.init();
+
 function TopHome() {
     return (
         <Grid container direction='row' className='top-home-cont'>
             <Grid item sm={6} xs={12}className='left'>
-                <h1>Find the best <h1 className='color'>talent</h1></h1>
+                <h1 data-aos='fade-right'>Find the best <h1 className='color'>talent</h1></h1>
             </Grid>
-            <Grid item sm={6} xs={12}className='right'>
+            <Grid item sm={6} xs={12}className='right' data-aos='fade-left' data-aos-delay='300'>
                 <hr/>
                 <p>Finding the right people and building high performing teams can be hard. Most
                     companies aren’t tapping into the abundance of global talent. We’re about to
