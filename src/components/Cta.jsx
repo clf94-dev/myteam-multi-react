dca48ea7e02493498d6c3ba5e894f9ec03def501
clf94-dev <clf94@hotmail.es>
2020-10-11 08:02:49 +0200

    added flip animaton Cta component

--- src/components/Cta.jsx
+++ src/components/Cta.jsx
@@ -2,9 +2,15 @@ import {Grid} from '@material-ui/core'
 import React from 'react'
 import {Link} from 'react-router-dom'
 
+
+import AOS from 'aos'
+import 'aos/dist/aos.css';
+
+AOS.init();
+
 function Cta() {
     return (
-        <Grid container direction='row' className='cta-cont'>
+        <Grid container direction='row' className='cta-cont' data-aos='flip-left'>
 
             <Grid item sm={8} xs={12} className='text-col'>
                 <h2>Ready to get started?</h2>
