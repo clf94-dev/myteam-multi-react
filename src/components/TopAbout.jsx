a97e25e8b98fd44e79c98697f69fbdddbcca1f24
clf94-dev <clf94@hotmail.es>
2020-10-11 08:05:24 +0200

    added animation TopAbout component

--- src/components/TopAbout.jsx
+++ src/components/TopAbout.jsx
@@ -1,13 +1,18 @@
 import {Grid} from '@material-ui/core'
 import React from 'react'
 
+import AOS from 'aos'
+import 'aos/dist/aos.css';
+
+AOS.init();
+
 function TopAbout() {
     return (
         <Grid container direction='row' className='topAbout-cont'>
             <Grid item md={6} xs={12}>
-                <h2>About</h2>
+                <h2 data-aos='fade-right'>About</h2>
             </Grid>
-            <Grid item md={6} xs={12}>
+            <Grid item md={6} xs={12} data-aos='fade-left' data-aos-delay='300'>
                 <hr/>
                 <p>We help companies build dynamic teams made up of top global talent. Using our network of passionate professionals we drive innovation and deliver incredible outcomes. Talented, diverse teams shape the best products and experiences. We’ll bring those teams to you.</p>
             </Grid>
